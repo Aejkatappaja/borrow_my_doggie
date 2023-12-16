21b2d84c66d7ea9f16857bf0997f6ad6caa8e4c0
Aejkatappaja <joverjlj@gmail.com>
2023-12-16 09:12:51 +0100

    refacto(Header): remove testing elements

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,8 +7,8 @@ interface HeaderProps {}
 export const Header: React.FC<HeaderProps> = ({}) => {
   return (
     <>
-      <header className='bg-dark-red w-full text-white '>
-        <nav className='mx-auto max-w-[1200px] border  py-2'>
+      <header className='w-full bg-dark-red text-white '>
+        <nav className='mx-auto max-w-[1200px] py-2'>
           <Image
             src='https://www.empruntemontoutou.com/wp-content/uploads/2021/08/empruntemontoutou-logo-x2_2021.svg'
             width={120}
@@ -16,18 +16,6 @@ export const Header: React.FC<HeaderProps> = ({}) => {
             alt='logo'
           />
         </nav>
-        <StickyPart />
-      </header>{' '}
-      <header className='w-full text-white'>
-        <nav className='bg-dark-red w-full max-w-[1222px] px-40 py-2'>
-          <Image
-            src='https://www.empruntemontoutou.com/wp-content/uploads/2021/08/empruntemontoutou-logo-x2_2021.svg'
-            width={120}
-            height={100}
-            alt='logo'
-          />
-        </nav>
-        <StickyPart />
       </header>
     </>
   );
