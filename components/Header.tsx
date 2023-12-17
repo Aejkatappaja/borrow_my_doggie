98a8b0c31a09fc75601144e463291899a7da890d
Aejkatappaja <joverjlj@gmail.com>
2023-12-17 23:33:30 +0100

    style(Header): rework ui

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import Image from 'next/image';
-import { StickyPart } from './StickyPart';
+import Link from 'next/link';
 
-interface HeaderProps {}
-
-export const Header: React.FC<HeaderProps> = ({}) => {
+export const Header: React.FC = () => {
   return (
     <>
-      <header className='w-full bg-dark-red text-white '>
-        <nav className='mx-auto max-w-[1200px] py-2'>
-          <Image
-            src='https://www.empruntemontoutou.com/wp-content/uploads/2021/08/empruntemontoutou-logo-x2_2021.svg'
-            width={120}
-            height={100}
-            alt='logo'
-          />
+      <header className='w-full bg-dark-red text-white'>
+        <nav className='mx-auto max-w-[1200px] px-2 py-3'>
+          <Link href='/'>
+            <Image
+              src='https://www.empruntemontoutou.com/wp-content/uploads/2021/08/empruntemontoutou-logo-x2_2021.svg'
+              width={120}
+              height={100}
+              alt='logo-emprunte-mon-toutou'
+            />
+          </Link>
         </nav>
       </header>
     </>
