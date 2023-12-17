77514750f6f45fea0d916c247f5e71d7227294dc
Aejkatappaja <joverjlj@gmail.com>
2023-12-17 23:35:14 +0100

    delete(Header): unused React.Fragment

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,19 +4,17 @@ import Link from 'next/link';
 
 export const Header: React.FC = () => {
   return (
-    <>
-      <header className='w-full bg-dark-red text-white'>
-        <nav className='mx-auto max-w-[1200px] px-2 py-3'>
-          <Link href='/'>
-            <Image
-              src='https://www.empruntemontoutou.com/wp-content/uploads/2021/08/empruntemontoutou-logo-x2_2021.svg'
-              width={120}
-              height={100}
-              alt='logo-emprunte-mon-toutou'
-            />
-          </Link>
-        </nav>
-      </header>
-    </>
+    <header className='w-full bg-dark-red text-white'>
+      <nav className='mx-auto max-w-[1200px] px-2 py-3'>
+        <Link href='/'>
+          <Image
+            src='https://www.empruntemontoutou.com/wp-content/uploads/2021/08/empruntemontoutou-logo-x2_2021.svg'
+            width={120}
+            height={100}
+            alt='logo-emprunte-mon-toutou'
+          />
+        </Link>
+      </nav>
+    </header>
   );
 };
