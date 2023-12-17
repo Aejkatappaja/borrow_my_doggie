a263adf35334a50a6f0100c07090d86c48037a85
Aejkatappaja <joverjlj@gmail.com>
2023-12-17 23:30:58 +0100

    add(utils): links & strings files to handle all website text and links

---
+++ utils/links.ts
@@ -0,0 +1,7 @@
+export const links = {
+  borrow_a_dog: '/emprunter-un-toutou',
+  entrust_your_dog: '/confier-votre-toutou',
+  trust_security: '/confiance-et-securite',
+  users_testimonials: '/temoignages',
+  our_history: '/notre-histoire',
+};
---
+++ utils/strings.ts
@@ -0,0 +1,11 @@
+export const strings = {
+  header: {},
+  sticky_header_part: {
+    borrow: 'Emprunter un toutou',
+    entrust: 'Confier votre toutou',
+    security: 'Sécurité',
+    testimonials: 'Témoignages',
+    history: 'Notre histoire',
+    button: 'Inscription gratuite',
+  },
+};
