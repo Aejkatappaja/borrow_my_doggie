a6de9957dbd67fed8e203b0d8e7fe3ce95cd5b22
Aejkatappaja <joverjlj@gmail.com>
2023-12-19 00:23:52 +0100

    fix(sticky_header): padding-left xl view

--- components/sticky-header-part/index.tsx
+++ components/sticky-header-part/index.tsx
@@ -11,7 +11,7 @@ export const StickyHeaderPart: React.FC = () => {
     <div
       className={`${raleway.className} top-0 z-50 hidden w-screen  bg-bright-red md:sticky md:flex md:px-3`}
     >
-      <nav className='mx-0 flex items-center justify-between tracking-tight text-white md:w-full md:pl-4 md:pr-20 lg:max-w-[1200px] xl:mx-auto xl:w-full xl:px-2 xl:px-4'>
+      <nav className=' mx-0 flex items-center justify-between tracking-tight text-white md:w-full md:pl-4 md:pr-20 lg:max-w-[1200px] xl:mx-auto xl:w-full xl:px-2'>
         {links?.map((item) => {
           const { label, href } = item;
           return <NavLinks key={label} href={href} label={label} />;
