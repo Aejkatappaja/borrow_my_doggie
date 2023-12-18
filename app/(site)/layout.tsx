dfc556b520ef74bbb28a56efcdac26813729e331
Aejkatappaja <joverjlj@gmail.com>
2023-12-18 23:54:01 +0100

    fix(site_layout): import name changed

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,4 +1,4 @@
-import { Header } from '@/components/Header';
+import { Header } from '@/components/header';
 import { StickyHeaderPart } from '@/components/sticky-header-part';
 
 export default function SiteLayout({
