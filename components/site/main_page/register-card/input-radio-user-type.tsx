bd22188775b76c18f29482c5c7c792b2eadacdbb
Aejkatappaja <joverjlj@gmail.com>
2023-12-18 23:54:57 +0100

    feat(components): add register_card & radio input component

---
+++ components/site/main_page/register-card/input-radio-user-type.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+interface InputRadioUserTypeProps {
+  id: string;
+  label: string;
+}
+
+export const InputRadioUserType: React.FC<InputRadioUserTypeProps> = ({
+  id,
+  label,
+}) => {
+  return (
+    <label htmlFor={id} className='rounded-xl border bg-white py-3 pl-4'>
+      <input type='radio' id={id} name='choice' className='accent-bright-red' />{' '}
+      <span className='pl-2'>{label}</span>
+    </label>
+  );
+};
