11d46911bac38759229f4b900fe6de952e1f89ea
Aejkatappaja <joverjlj@gmail.com>
2023-12-18 23:11:00 +0100

    add(utils_strings): missing texts

--- utils/strings.ts
+++ utils/strings.ts
@@ -2,6 +2,7 @@ export const strings = {
   header: {
     contact: 'Contactez-nous',
     member: 'Espace Membre',
+    mobile_menu: 'Menu',
   },
   sticky_header_part: {
     borrow: 'Emprunter un toutou',
@@ -10,5 +11,18 @@ export const strings = {
     testimonials: 'Témoignages',
     story: 'Notre histoire',
     button: 'Inscription gratuite',
+  },
+  main_page: {
+    title: 'Trouvez un emprunteur ou un toutou proche de chez vous.',
+    subtitle: 'Pour des promenades, des gardes et du bonheur !',
+    card: {
+      select: 'Vous souhaitez :',
+      first_choice: 'Faire garder votre toutou 🐶',
+      second_choice: 'Passer du temps avec un toutou 🐕',
+      button: 'Inscription gratuite',
+      span_start: "Rejoignez près d'",
+      members_number: '1 million de membres',
+      span_end: 'partout en France !',
+    },
   },
 };
