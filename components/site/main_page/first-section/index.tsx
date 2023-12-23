1b259edd4d4c211ac7dce5963af34f6ceb8a409b
Aejkatappaja <joverjlj@gmail.com>
2023-12-24 00:49:27 +0100

    fix(page): import name

--- components/site/main_page/first-section/index.tsx
+++ components/site/main_page/first-section/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { RegisterCard } from '../register-card';
+import { RegisterCard } from './register-card';
 import Image from 'next/image';
 import { strings } from '@/utils/strings';
 Image;
