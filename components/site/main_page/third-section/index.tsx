50bea32c59d4cddf232249c0129aa08ece57d23d
Aejkatappaja <joverjlj@gmail.com>
2023-12-25 18:56:45 +0100

    fix(third_section):  import component

--- components/site/main_page/third-section/index.tsx
+++ components/site/main_page/third-section/index.tsx
@@ -1,14 +1,19 @@
 import { strings } from '@/utils/strings';
 import { Title } from '../../ui/title';
+import { SubHeading } from '../../ui/sub-heading';
 
 export default function ThirdSection() {
+  const {
+    main_page: {
+      third_section: { heading, title },
+    },
+  } = strings;
+
   return (
     <div className='special-bg relative mt-10 flex flex-col items-center gap-10 pb-14 md:mt-0'>
       <div className='flex flex-col items-center justify-center gap-3 px-1'>
-        <strong className='text-2xl font-bold tracking-tight'>
-          Présentation du concept
-        </strong>
-        <Title text={strings.site} center={true} />
+        <SubHeading text={heading} />
+        <Title text={title} center={true} />
       </div>
       <iframe
         src='https://www.youtube.com/embed/oQrIZsqAbtg'
