a72a9541bc3e5d34890c2bd6a30c8eb1d3f8fc32
Aejkatappaja <joverjlj@gmail.com>
2023-12-26 22:25:40 +0100

    add(strings): more typo

--- utils/strings.ts
+++ utils/strings.ts
@@ -43,8 +43,53 @@ export const strings = {
       title: 'Emprunte Mon Toutou',
     },
     fourth_section: {
-      heading: 'Comment ça marche',
+      heading: 'Comment ça marche ?',
       title: 'Simplicité et génerosité',
+      cards: {
+        profile: {
+          title: 'Créez votre profil',
+          content:
+            'Inscrivez-vous et remplissez votre profil ou le profil de votre toutou',
+        },
+        happiness: {
+          title: 'Trouvez votre bonheur',
+          content:
+            "L'emprunteur ou le toutou qui éclairera votre quotidien vous attend à deux pattes de chez vous.",
+        },
+        meeting: {
+          title: 'Rencontrons-nous',
+          content:
+            'Prenez le temps de faire connaissance, puis partagez le bonheur des toutous, sans limites !',
+        },
+      },
+    },
+    fifth_section: {
+      title: 'Tous les jours de nouveaux toutous se joignent à nous !',
+    },
+    sixth_section: {
+      title: 'Du bonheur pour tout le monde :',
+      cards: {
+        owner: {
+          header: 'Maîtres',
+          first_title: 'Rejoignez notre communauté et gagnez-y :',
+          list: [
+            {
+              li: "De l'aide pour s'occuper de votre toutou quand vous en avez besoin, par des personnes de confiance, disponibles et près de chez vous",
+            },
+            { li: 'Votre toutou est encore plus sociable et heureux' },
+            {
+              li: "Une protection pour votre toutou grâce à nos nombreuses mesures de sécurité, notre assurance vétérinaire et ligne d'assistance vétérinaire 24h/24",
+            },
+          ],
+
+          second_title:
+            "L'inscription et la création de votre profil sont entièrement gratuites.",
+          first_text:
+            "Vous pouvez ensuite choisir de devenir membre premiummoins de 5 € par mois, afin de bénéficier d'une assurance vétérinaire, de la messagerie illimitée, de la ligne d'assistance vétérinaire 24h/24 et d'une assurance dommages.",
+          second_text:
+            'Votre cotisation est garantie satisfait ou remboursée pendant 1 mois, pour ne plus avoir à payer un pet sitter ou un chenil !',
+        },
+      },
     },
   },
 };
