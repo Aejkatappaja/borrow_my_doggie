b1ef00bba36e9bf9ed8a58927f79ac000d915e8c
Aejkatappaja <joverjlj@gmail.com>
2023-12-27 00:05:30 +0100

    add(main_page): SixthSection

--- components/site/main_page/sixth-section/page.tsx
+++ components/site/main_page/sixth-section/page.tsx
@@ -1,50 +1,25 @@
 import { strings } from '@/utils/strings';
 import { Title } from '../../ui/title';
+import { Card } from './card';
 
-export default function SixthSection() {
-  const array = strings.main_page.sixth_section.cards.owner.list;
-  console.log(array);
+async function fetchData() {
+  const data1 = await strings.main_page.sixth_section.cards.owner;
+  const data2 = await strings.main_page.sixth_section.cards.borrower;
+  const data3 = await strings.main_page.sixth_section.cards.doggos;
+
+  return [data1, data2, data3];
+}
+
+export default async function SixthSection() {
+  const [data1, data2, data3] = await fetchData();
+
   return (
-    <div className='flex flex-col items-center justify-center gap-10 bg-light-grey px-6 py-12 pb-14 font-nunito md:mt-0  md:px-0'>
+    <div className='flex flex-col items-center justify-center gap-10 bg-light-grey px-6 py-12 pb-14 font-nunito md:mt-0 md:px-0'>
       <Title text={strings.main_page.sixth_section.title} />
-      <div className='flex gap-1'>
-        <div className='h-[705px] w-[390px] rounded-2xl border'>
-          <header className='rounded-t-2xl bg-turquoise py-4'>
-            <Title
-              center
-              variant='card'
-              text={strings.main_page.sixth_section.cards.owner.header}
-            />
-          </header>
-          <section className='px-8 py-4'>
-            <h2>{strings.main_page.sixth_section.cards.owner.first_title}</h2>
-            <ul className='list-tick px-4'>
-              {array.map((item: { li: string }, index: number) => {
-                const { li } = item;
-
-                return <li key={index}>{li}</li>;
-              })}
-            </ul>
-          </section>
-        </div>{' '}
-        <div className='w-[390px] border'>
-          <header className='rounded-t-2xl border-2 bg-turquoise py-4'>
-            <Title
-              center
-              variant='card'
-              text={strings.main_page.sixth_section.cards.owner.header}
-            />
-          </header>
-        </div>{' '}
-        <div className='w-[390px] border'>
-          <header className='rounded-t-2xl border-2 bg-turquoise py-4'>
-            <Title
-              center
-              variant='card'
-              text={strings.main_page.sixth_section.cards.owner.header}
-            />
-          </header>
-        </div>
+      <div className='flex flex-col gap-6 md:flex-row md:gap-2 md:px-6'>
+        <Card color='bg-turquoise' data={data1} />
+        <Card color='bg-light-green' data={data2} />
+        <Card color='bg-bright-orange' data={data3} />
       </div>
     </div>
   );
