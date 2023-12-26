8343d5e941ad9049c760eace876b7f04c570f2d1
Aejkatappaja <joverjlj@gmail.com>
2023-12-26 22:27:14 +0100

    refacto(components_Title): add variant

--- components/site/ui/title.tsx
+++ components/site/ui/title.tsx
@@ -3,15 +3,20 @@ import React from 'react';
 interface TitleProps {
   text: string;
   center?: boolean;
+  variant?: string;
 }
 
-export const Title: React.FC<TitleProps> = ({ text, center }) => {
+export const Title: React.FC<TitleProps> = ({ text, center, variant }) => {
   let text_center: string = '';
+  let text_color: string = 'text-bright-red';
   if (center) {
     text_center = 'text-center';
   }
+  if (variant === 'card') {
+    text_color = 'text-white';
+  }
   return (
-    <h1 className={`${text_center} font-doggo text-3xl text-bright-red`}>
+    <h1 className={`${text_center} font-doggo text-3xl ${text_color}`}>
       {text}
     </h1>
   );
