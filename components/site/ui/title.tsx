0203cd144937fa0f441f034ee23b65fc1351890c
Aejkatappaja <joverjlj@gmail.com>
2023-12-27 23:50:21 +0100

    refacto(components_Title): added size property

--- components/site/ui/title.tsx
+++ components/site/ui/title.tsx
@@ -4,19 +4,31 @@ interface TitleProps {
   text: string;
   center?: boolean;
   variant?: string;
+  size?: string;
 }
 
-export const Title: React.FC<TitleProps> = ({ text, center, variant }) => {
+export const Title: React.FC<TitleProps> = ({
+  text,
+  center,
+  variant,
+  size,
+}) => {
   let text_center: string = '';
   let text_color: string = 'text-bright-red';
+  let text_size: string = '';
   if (center) {
     text_center = 'text-center';
   }
   if (variant === 'card') {
     text_color = 'text-white text-lg';
   }
+  if (size === 'xl') {
+    text_size = 'text-3xl';
+  }
   return (
-    <h1 className={`${text_center} font-doggo md:text-3xl ${text_color}`}>
+    <h1
+      className={`${text_center} ${text_size} font-doggo md:text-3xl ${text_color}`}
+    >
       {text}
     </h1>
   );
